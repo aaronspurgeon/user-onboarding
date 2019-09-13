60bfc392d1b1849c4e94068c03b133733be904de
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-09-12 20:01:09 -0400

    MVP complete, have the users rendering from the api call

--- onboard/src/Components/Form.js
+++ onboard/src/Components/Form.js
@@ -8,16 +8,31 @@ const UserForm = ({ errors, touched, status }) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        
-    })
+        if (status) {
+            setData([ ...data, status ])
+        }
+    }, [status])
 
     return (
         <Form>
             {touched.user && errors.user && <p className="error">{errors.user}</p>}
             <Field type="text" name="user" placeholder="Name"></Field>
+
+            {touched.email && errors.email && <p className="error">{errors.email}</p>}
             <Field type="email" name="email" placeholder="Email"></Field>
+
+            {touched.password && errors.password && <p className="error">{errors.password}</p>}
             <Field type="password" name="password" placeholder="Password"></Field>
+
+            {touched.terms && errors.terms && <p className="error">{errors.terms}</p>}
             <Field type="checkbox" name="terms"></Field>
+
+            <button type="submit">Submit</button>
+
+            {data.map((data) => (
+                <div>User: {data.user}</div>
+            ))}
+            
         </Form>
     )
 }
@@ -36,6 +51,16 @@ export default withFormik({
         email: yup.string().required('Email is required!'),
         password: yup.string().required('Password is required!'),
         terms: yup.boolean().oneOf([true], 'Must agree to terms of service')
-    })
+    }),
+
+    handleSubmit: (values, { setStatus }) => {
+        axios.post('https://reqres.in/api/users', values)
+            .then((res) => {
+                setStatus(res.data)
+            })
+            .catch((err) => {
+                console.log('Error:', err);
+            })
+    }
 })(UserForm);
 
